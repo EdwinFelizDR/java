f2c9c43abeabc8b923670c9ed4ad69521d0fcb1c
EdwinFelizDR <efeliz12lg@gmail.com>
2024-07-08 10:58:34 -0400

    Practice Assignment - Array: task3 to task6 (done)

--- PA30321/CoreJavaBasics/src/com/perscholas/java_basics/PA30371.java
+++ PA30321/CoreJavaBasics/src/com/perscholas/java_basics/PA30371.java
@@ -32,8 +32,56 @@ public class PA30371 {
         }
     }
 
+    //creates an array of String Print out the array length.
+    // Make a copy using the clone() method. Use the Arrays.toString()
+    static void task3(){
+        String[] sourceArray={"red", "green", "blue", "yellow"};
+        String[] targetArray = sourceArray.clone();
+        System.out.println(sourceArray.length);
+        System.out.println("This is the source array: "+Arrays.toString(sourceArray));
+        System.out.println("This is the target array: "+Arrays.toString(targetArray));
+    }
+
+    //creates an integer array with 5 elements
+    //Print out the value at the first index and the last index
+    static void task4(){
+        int[] intArray = {43,67,3,9,12};
+        System.out.println(intArray[0]);
+        System.out.println(intArray[intArray.length-1]);
+
+        //Now try printing the value at index = length
+        System.out.println(intArray[intArray.length]);
+
+    }
+
+    static void task5(){
+        int[] intArr = new int[5];
+        for (int i=0;i<intArr.length;i++){
+            intArr[i] = i;
+        }
+        System.out.println(Arrays.toString(intArr));
+    }
+
+    static void task6(){
+        int[] intArr = new int[5];
+        for (int i=0;i<intArr.length;i++){
+            intArr[i] = i*2;
+        }
+        System.out.println(Arrays.toString(intArr));
+    }
+
+    static void task7(){
+        
+    }
+
     public static void main(String[] args) {
         //task1();
         //task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        task7();
+
     }
 }
